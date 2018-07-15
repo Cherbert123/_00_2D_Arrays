ce1fb88a9cde9c86f9b2b24eb3e01b6aaaa2ab1e
Charlie <league@WTS13.attlocal.net>
2018-07-15 12:58:24 -0700

    Mazes

--- src/_04_Maze_Maker/MazeMaker.java
+++ src/_04_Maze_Maker/MazeMaker.java
@@ -1,8 +1,9 @@
 package _04_Maze_Maker;
-
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
+
+import org.omg.Messaging.SyncScopeHelper;
 
 
 public class MazeMaker{
@@ -14,62 +15,97 @@ public class MazeMaker{
 	
 	private static Random randGen = new Random();
 	private static Stack<Cell> uncheckedCells = new Stack<Cell>();
+	static int wid = 0;
+	static int hgt = 0;
 	
+	static ArrayList<Cell> cells = new ArrayList<Cell>();
 	
 	public static Maze generateMaze(int w, int h){
 		width = w;
 		height = h;
+		wid = w;
+		hgt = h;
 		maze = new Maze(width, height);
 		
-		//4. select a random cell to start
-		
-		
-		//5. call selectNextPath method with the randomly selected cell
-		
-		
+		//select a random cell to start
+		Cell start = maze.getCell(randGen.nextInt(w),randGen.nextInt(h));
+		//call selectNextPath method with the randomly selected cell
+		selectNextPath(start);
 		return maze;
+		
 	}
 
-	//6. Complete the selectNextPathMethod
 	private static void selectNextPath(Cell currentCell) {
-		//A. mark cell as visited
+		currentCell.setBeenVisited(true);
 
-		//B. check for unvisited neighbors using the cell
-		
-		//C. if has unvisited neighbors,
-		
-			//C1. select one at random.
-			
-			//C2. push it to the stack
-		
-			//C3. remove the wall between the two cells
+		// check for unvisited neighbors
+		int curX = currentCell.getX();
+		int curY = currentCell.getY();
 
-			//C4. make the new cell the current cell and mark it as visited
+		cells = getUnvisitedNeighbors(maze.getCell(curX, curY));
+
+		// if has unvisited neighbors,
+		// select one at random.
+		// push it to the stack
+		// remove the wall between the two cells
+		// make the new cell the current cell and mark it as visited
+
+		Random rand = new Random();
+
+		if (!cells.isEmpty()) {
+
+			int ran = rand.nextInt(cells.size());
+			uncheckedCells.push(cells.get(ran));
+			removeWalls(currentCell, cells.get(ran));
+			currentCell = cells.get(ran);
+			selectNextPath(currentCell);
 			
-			
-		//D. if all neighbors are visited
-		
-			//D1. if the stack is not empty
-			
-				// D1a. pop a cell from the stack
-		
-				// D1b. make that the current cell
-				
-			
-		
+
+		} else {
+			if (!uncheckedCells.isEmpty()) {
+				currentCell = uncheckedCells.pop();
+				selectNextPath(currentCell);
+			}
+		}
 	}
 
-	//7. Complete the remove walls method.
-	//   This method will check if c1 and c2 are adjacent.
-	//   If they are, the walls between them are removed.
 	private static void removeWalls(Cell c1, Cell c2) {
-		
+		if(c1.getX() + 1 == c2.getX()) {
+			c1.setEastWall(false);
+			c2.setWestWall(false);
+		}else if(c1.getX() - 1 == c2.getX()) {
+			c1.setWestWall(false);
+			c2.setEastWall(false);
+		}else if(c1.getY() + 1 == c2.getY()) {
+			c1.setNorthWall(false);
+			c2.setSouthWall(false);
+		}else if(c1.getY() - 1 == c2.getY()) {
+			c1.setSouthWall(false);
+			c2.setNorthWall(false);
+		}
 	}
-	
-	//8. Complete the getUnvisitedNeighbors method
-	//   Any unvisited neighbor of the passed in cell gets added
-	//   to the ArrayList
+
 	private static ArrayList<Cell> getUnvisitedNeighbors(Cell c) {
-		return null;
+
+		ArrayList<Cell> cells = new ArrayList<Cell>();
+
+		int curX = c.getX();
+		int curY = c.getY();
+
+		System.out.println("X: " + curX + " Y: " + curY);
+
+
+		if (curX > 0 && curX < width - 1 && !maze.getCell(curX + 1, curY).hasBeenVisited()) {
+			cells.add(maze.getCell(curX + 1, curY));
+		} else if (curX < width && curX > 0 && !maze.getCell(curX - 1, curY).hasBeenVisited()) {
+			cells.add(maze.getCell(curX - 1, curY));
+		}
+		if (curY > 0 && curY < height - 1 && !maze.getCell(curX, curY + 1).hasBeenVisited()) {
+			cells.add(maze.getCell(curX, curY + 1));
+		} else if (curY < height && curY > 0 && !maze.getCell(curX, curY - 1).hasBeenVisited()) {
+			cells.add(maze.getCell(curX, curY - 1));
+		}
+		return cells;
+		
 	}
 }
